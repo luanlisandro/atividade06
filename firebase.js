4cf6e0ab1bcb05619053f90b882e9ebeadb52354
luanlisandro <luanlisandro1@gmail.com>
2026-04-23 08:33:52 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4nmNUBn5Had70mTG6pLa9MuIDtk9xPm0",
+  authDomain: "atividade06-fe6fa.firebaseapp.com",
+  projectId: "atividade06-fe6fa",
+  storageBucket: "atividade06-fe6fa.firebasestorage.app",
+  messagingSenderId: "864912959558",
+  appId: "1:864912959558:web:4ef73be03247fa23b80247"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
